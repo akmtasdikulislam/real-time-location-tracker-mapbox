168920da49993a90e83deab73f5363ab59a425da
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2026-03-17 00:22:13 +0600

    Remove unused SSL options and simplify server response messages

--- server/index.js
+++ server/index.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const app = express();
 const https = require("https");
-const os = require("os");
-// const fs = require("fs");
 const { Server } = require("socket.io");
-
-// // SSL
-// const sslOptions = {
-//   key: fs.readFileSync("./server-ssl-key.pem"),
-//   cert: fs.readFileSync("./server-ssl.pem"),
-// };
 
 // Create HTTPS Express Server
 const expressServer = https.createServer(app);
@@ -21,8 +13,6 @@ const io = new Server(expressServer, {
   },
 });
 
-// Current Network Address
-const ipAddress = Object.values(os.networkInterfaces())[0][1].address;
 const port = process.env.PORT || 3001;
 
 // Storing each user's location data
@@ -63,9 +53,9 @@ io.on("connection", (socket) => {
 });
 
 app.get("/", (req, res) => {
-  res.send(`Server is running at https://${ipAddress}:${port}`);
+  res.send(`Server is running`);
 });
 
 expressServer.listen(port, () => {
-  console.log(`Server is running at https://${ipAddress}:${port}`);
+  console.log(`Server is running`);
 });
