1469de2e8bc29c23a6930a95399c343a607b50e4
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2026-03-17 00:14:35 +0600

    Refactor server setup to remove SSL options and update port configuration

--- server/index.js
+++ server/index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 const https = require("https");
 const os = require("os");
-const fs = require("fs");
+// const fs = require("fs");
 const { Server } = require("socket.io");
 
-// SSL
-const sslOptions = {
-  key: fs.readFileSync("./server-ssl-key.pem"),
-  cert: fs.readFileSync("./server-ssl.pem"),
-};
+// // SSL
+// const sslOptions = {
+//   key: fs.readFileSync("./server-ssl-key.pem"),
+//   cert: fs.readFileSync("./server-ssl.pem"),
+// };
 
 // Create HTTPS Express Server
-const expressServer = https.createServer(sslOptions, app);
+const expressServer = https.createServer(app);
 
 // Create Socket Server
 const io = new Server(expressServer, {
@@ -23,7 +23,7 @@ const io = new Server(expressServer, {
 
 // Current Network Address
 const ipAddress = Object.values(os.networkInterfaces())[0][1].address;
-const port = 5000;
+const port = process.env.PORT || 3001;
 
 // Storing each user's location data
 let locationData = {};
